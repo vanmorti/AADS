4af773a23b8980f0ac6806d720481fa78c21bffb
Iva <142234211+vanmorti@users.noreply.github.com>
2025-03-06 17:54:20 +0300

    Update Алгоритм Краскала.cpp

--- Алгоритм Краскала.cpp
+++ Алгоритм Краскала.cpp
@@ -21,59 +21,57 @@ typedef long long ll;
 Если они уже находятся в одном множестве, пропускаем ребро. Если они находятся в разных множествах (функция unite вернуля true), 
 добавляем ребро в остовное дерево (в момент, когда мы использовали unite, множества с данными вершинами уже объединились).
 */
-struct Dsu {
-    vector<int> l;
-    vector<int> r;
-    void set(int n) {
-        l.resize(n);
-        r.resize(n, 1);
-        for (int i = 0; i < n; ++i) {
-            l[i] = i;
-        }
-    }
-    int leader(int v) {
-        if (l[v] == v) {
-            return v;
-        }
-        return l[v] = leader(l[v]);
-    }
-    bool unite(int a, int b) {
-        a = leader(a); b = leader(b);
-        if (a == b) {
-            return false;
-        }
-        if (r[a] > r[b]) {
-            swap(a, b);
-        }
-        r[b] = max(r[b], r[a] + 1);
-        l[a] = b;
-        return true;
-    }
+struct Dsu{    
+    vector<int> l; // лидеры
+	vector<int> r; // ранги деревьев
+	void init(int n) {
+		l.resize(n);
+		r.resize(n, 1);
+		for (int i = 0; i < n; ++i) {
+			l[i] = i;
+		}
+	}
+	int leader(int v) {
+		if (l[v] == v) {
+			return v;
+		}
+		return l[v] = leader(l[v]);
+	}
+	bool unite(int a, int b) {
+		a = leader(a); b = leader(b);
+		if (a == b) {
+			return false;
+		}
+		if (r[a] > r[b]) {
+			swap(a, b);
+		}
+		r[b] = max(r[b], r[a] + 1);
+		l[a] = b;
+		return true;
+	}
 };
 
 struct Edge {
-    int f, t, w;
+    int from, to, weight;
 };
-
-bool cmp(Edge a, Edge b) {
-    return a.w < b.w;
-}
 
 signed main(){
     int n, m;
     cin >> n >> m;
     Dsu d; 
-    d.set(n);
+    d.init(n);
     vector<Edge> edges(m);
     for (Edge &e : edges) {
-        cin >> e.f >> e.t >> e.w;
-        e.f--; e.t--;
+        cin >> e.from >> e.to >> e.weight;
+        e.from--; e.to--;
     }
-    sort(edges.begin(), edges.end(), cmp);
+    sort(edges.begin(), edges.end(), [](Edge a, Edge b) {
+        return a.weight < b.weight;
+    });
     int ans = 0;
     for (Edge &e : edges) {
-        if (d.unite(e.f, e.t)) {
-            ans += e.w;
+        if (d.unite(e.from, e.to)) {
+            ans += e.weight;
         }
     }
     cout << ans << '\n';
